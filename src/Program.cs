6199233bee331553da4fa27fef1b2fb49359655c
NibbaThomAat <thomdejong99@hotmail.nl>
2018-09-17 11:10:03 +0200

    Basis functionaliteit

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,33 @@ namespace src
 	{
 		static void Main(string[] args)
 		{
+			var brick = new Brick<Sensor, Sensor, Sensor, Sensor>("WiFi");
+			ConsoleKeyInfo cki;
+			try
+			{
+				brick.Connection.Open();
+				do
+				{
+					cki = Console.ReadKey(true);
+					switch (cki.Key)
+					{
+						case ConsoleKey.UpArrow:
+							brick.MotorA.On(30);
+							break;
+						case ConsoleKey.DownArrow:
+							brick.MotorA.On(-30);
+							break;
+					}
+				} while (!cki.Key.ToString().Equals("Escape"));
+				if (brick.MotorA.IsRunning())
+				{
+					brick.MotorA.Off();
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
 		}
 	}
 }
