52c4e811555e9808aabdaffb1fdc89ee5510dbe6
NibbaThomAat <thomdejong99@hotmail.nl>
2018-09-19 10:55:40 +0200

    Klasse voor de afstandssensor toegevoegd

--- src/Program.cs
+++ src/Program.cs
@@ -18,7 +18,20 @@ namespace src
 				Console.WriteLine(e.Message);
 				return;
 			}
-			//Programma hier
+			// Programma hier
 		}
+	}
+}
+
+class DistanceSensor : Sensor
+{
+	/**
+	 *
+	 * Reads the distance in centimeters, returns a float.
+	 *
+	 */
+	public float Read()
+	{
+		return float.Parse(this.ReadAsString());
 	}
 }
